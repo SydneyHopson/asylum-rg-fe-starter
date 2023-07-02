cbf0678c1401004e24b20a204a07e1a9670a63f1
Sydney.Hopson <Karatesyd@icloud.com>
2023-07-02 16:47:02 -0400

    added all imports includinng pageNavagation, created graphic section and attached img and paragraphs, created read more button

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-// ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
 import { useHistory } from 'react-router-dom';
-// for the purposes of testing PageNav
-// import PageNav from '../../common/PageNav';
+import PageNav from '../../common/PageNav';
 
 function RenderLandingPage(props) {
   const scrollToTop = () => {
@@ -32,7 +30,26 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      <div className="graphs-section">
+        <div>
+          <img src={GrantRatesByOfficeImg} alt="Grant Rates By Office" />
+          <p>Search Grant Rates By Office</p>
+        </div>
+
+        <div>
+          <img
+            src={GrantRatesByNationalityImg}
+            alt="Grant Rates By Nationality"
+          />
+          <p>Search Grant Rates By Nationality</p>
+        </div>
+
+        <div>
+          <img src={GrantRatesOverTimeImg} alt="Grant Rates Over Time" />
+          <p>Search Grant Rates Over Time</p>
+        </div>
+      </div>
+
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -59,14 +76,53 @@ function RenderLandingPage(props) {
           </h3>
         </div>
       </div>
-      <div>
-        {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
-        <p onClick={() => scrollToTop()} className="back-to-top">
-          Back To Top ^
-        </p>
+
+      {/* Bottom Section: Add code here for the graphs section for your first ticket */}
+      <div className="bottom-section">
+        <h2>Systemic Disparity Insights</h2>
+        <div className="bottom-container">
+          <div className="bottom-container-outer-left">
+            <h3>36%</h3>
+            <p>
+              By the end of the Trump administration, the average asylum office
+              grant rate had fallen 36 percent from an average of 44 percent in
+              fiscal year 2016 to 28 percent in fiscal year 2020.
+            </p>
+          </div>
+
+          <div className="bottom-container-middle">
+            <h3>5%</h3>
+            <p>
+              The New York asylum office grant rate dropped to 5 percent in
+              fiscal year 2020.
+            </p>
+          </div>
+
+          <div className="bottom-container-right">
+            <h3>6X lower</h3>
+            <p>
+              Between fiscal year 2017 and 2020, the New York asylum office’s
+              average grant rate was six times lower than the San Francisco
+              asylum office.
+            </p>
+          </div>
+
+          <div className="bottom-read-more-button-middle">
+            <button
+              type="default"
+              style={{ backgroundColor: 'gray', color: 'white' }}
+            >
+              Read More
+            </button>
+          </div>
+        </div>
       </div>
+
+      <p onClick={() => scrollToTop()} className="back-to-top">
+        Back To Top ^
+      </p>
     </div>
   );
 }
+
 export default RenderLandingPage;
